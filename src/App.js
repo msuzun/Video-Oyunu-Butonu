4d608827901c507ead3c54c99140424d3fa9c08d
msuzun <sevki.uzun53@hotmail.com>
2024-11-24 19:25:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [gameRunning, setGameRunning] = useState(false);
+  const handleClick= () =>{
+    setGameRunning(!gameRunning)
+  }
+  let btn = gameRunning ? <button onClick={handleClick} className="video-game-button">Play</button> : <button onClick={handleClick} className="video-game-button">Pause</button>
+  /* Challenge
+  
+    Kullanıcının bir video oyununu oynatmasını ve duraklatmasını sağlayacak buton oluşturuyorsunuz. Göreviniz bunu ayarlamak ve aşağıdaki div'e (satır 26) aşağıdaki gibi yerleştirmektir: 
+            
+        1. Buton, "video-game-button" şeklinde bir className almalıdır
+        
+        2. Kullanıcı butona tıkladığında, gameRunning durumunun boolean değeri şu anda olduğu değerin tersine ayarlanmalıdır (true'dan false'a veya tam tersi).
+            
+        3. Eğer gameRunning doğru ise, buton "Pause" kelimesini göstermelidir. Eğer gameRunning değeri false ise, buton "Play" kelimesini göstermelidir. Başka bir deyişle:
+
+          
+             ┌─────────────┐     ------------>     ┌─────────────┐            
+             │     Play    │         click         │    Pause    │
+             └─────────────┘     <------------     └─────────────┘
+           gameRunning = false                    gameRunning = true 
+           (oyun duraklatıldı)                   (oyun oynanıyor)
+             
+        4. Bu görevleri doğru bir şekilde tamamlarsanız, butona tıkladığınızda bazı güzel görsel efektlere sahip olmalı ve çalışmamızın arka planı otomatik olarak açık mavi bir arka planla değiştirilmelidir.
+*/
+  return <div>
+    {btn}
+  </div>;
+}
